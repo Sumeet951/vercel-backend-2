5716e45ba09054718af76bf057e0e406f13ffee5
Sumeet951 <sg297979@gmail.com>
2025-11-12 20:09:37 +0530

    it working

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app= express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-    origin:'http://localhost:5173', // frontend url accesing
+    origin:'https://vercel-backend-2-six.vercel.app/', // frontend url accesing
     credentials:true
 }));
 app.use(morgan('dev'));
