5b7578a8bf77129ab0a3719cf0800b6ddf64df97
Sumeet951 <sg297979@gmail.com>
2025-11-13 10:03:03 +0530

    working

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 import app from "./app.js";
 import databaseconnect from "./config/dbConnection.js";
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, async () => {
-  await databaseconnect();
-  console.log(`App is running at http://localhost:${PORT}`);
-})
+const startServer = async () => {
+  try {
+    await databaseconnect();
+    app.listen(PORT, () => console.log("Server started"));
+  } catch (err) {
+    console.log("DB Error", err);
+  }
+};
+
+startServer();
